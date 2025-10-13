d5441eb67df52a6d15ba5739a8fce98278fd7438
NIGHTZIN <hatsunemikutoro@gmail.com>
2025-10-13 19:28:08 -0300

    add layers

--- code/main.py
+++ code/main.py
@@ -22,7 +22,7 @@ class Game:
         self.setup()
 
         # SPRITES
-        self.player = Player((500, 300), self.all_sprites, self.collision_sprites)
+
     def setup(self):
         map = load_pygame('S:\\Vscode\\aBUBU\\VampireSurvivorsPyGAME\\Vampire survivor\\data\\maps\\world.tmx')
         for x,y, image in map.get_layer_by_name('Ground').tiles():
@@ -32,6 +32,10 @@ class Game:
 
         for obj in map.get_layer_by_name('Collisions'):
             CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+
+        for obj in map.get_layer_by_name('Entities'):
+            if obj.name == "Player":
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
 
     def run(self):
         while self.running:
--- code/sprites.py
+++ code/sprites.py
@@ -5,6 +5,7 @@ class Sprite(pygame.sprite.Sprite):
         super().__init__(group)
         self.image = surf
         self.rect = self.image.get_frect(topleft=pos)
+        self.ground = True
 
 class CollisionSprite(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
