6a84c1c723fa212c86f2ac866703819e65e51a6d
NIGHTZIN <hatsunemikutoro@gmail.com>
2025-10-14 17:51:04 -0300

    gun

--- code/main.py
+++ code/main.py
@@ -14,14 +14,35 @@ class Game:
         pygame.display.set_caption('Survivor')
         self.clock = pygame.time.Clock()
         self.running = True
+        self.load_images()
 
         # GROUPS
         self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
 
         self.setup()
 
-        # SPRITES
+        # GUN TIMER
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.gun_cooldown = 100
+
+    def load_images(self):
+        self.bullet_surf = pygame.image.load('S:\\Vscode\\aBUBU\VampireSurvivorsPyGAME\\Vampire survivor\\images\gun\\bullet.png').convert_alpha()
+
+    def input(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            pos = self.gun.rect.center + self.gun.player_direction * 50
+            Bullet(self.bullet_surf, pos, self.gun.player_direction, (self.all_sprites, self.bullet_sprites))
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    def gun_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time >= self.gun_cooldown:
+                self.can_shoot = True
 
     def setup(self):
         map = load_pygame('S:\\Vscode\\aBUBU\\VampireSurvivorsPyGAME\\Vampire survivor\\data\\maps\\world.tmx')
@@ -36,6 +57,7 @@ class Game:
         for obj in map.get_layer_by_name('Entities'):
             if obj.name == "Player":
                 self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                self.gun = Gun(self.player, self.all_sprites)
 
     def run(self):
         while self.running:
@@ -47,6 +69,8 @@ class Game:
                     self.running = False
 
             # update
+            self.gun_timer()
+            self.input()
             self.all_sprites.update(dt)
             # draw
             self.display_surface.fill('black')
