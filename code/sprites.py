5405182301bbbf17850e8cac526bf5610bb66359
NIGHTZIN <hatsunemikutoro@gmail.com>
2025-10-14 18:23:00 -0300

    adicionado enemies

--- code/sprites.py
+++ code/sprites.py
@@ -67,3 +67,48 @@ class Enemy(pygame.sprite.Sprite):
     def __init__(self, pos, frames, groups, player, collision_sprites):
         super().__init__(groups)
         self.player = player
+
+        # image
+        self.frames, self.frame_index = frames, 0
+        self.image = self.frames[self.frame_index]
+        self.animation_speed = 6
+
+        # rect
+        self.rect = self.image.get_frect(center=pos)
+        self.hitbox = self.rect.inflate(-20,-40)
+        self.collision_sprites = collision_sprites
+        self.direction = pygame.Vector2()
+        self.speed = 350
+
+
+    def animate(self, dt):
+        self.frame_index += self.animation_speed * dt
+        self.image = self.frames[int(self.frame_index) % len(self.frames)]
+
+    def move(self, dt):
+        # get direction
+        player_pos = pygame.Vector2(self.player.rect.center)
+        enemy_pos = pygame.Vector2(self.rect.center)
+        self.direction = (player_pos - enemy_pos).normalize()
+
+        # update the rect position + collision
+        self.hitbox.centerx += self.direction.x  * self.speed * dt
+        self.collisions('horizontal')
+        self.hitbox.centery += self.direction.y * self.speed * dt
+        self.collisions('vertical')
+        self.rect.center = self.hitbox.center
+    def collisions(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.hitbox):
+                if direction == 'horizontal':
+                    if self.direction.x > 0: self.hitbox.right = sprite.rect.left
+                    if self.direction.x < 0: self.hitbox.left = sprite.rect.right
+                else:
+                    if self.direction.y < 0: self.hitbox.top = sprite.rect.bottom
+                    if self.direction.y > 0: self.hitbox.bottom = sprite.rect.top
+
+
+
+    def update(self, dt):
+        self.move(dt)
+        self.animate(dt)
